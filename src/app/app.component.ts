1c11f789aed5e07c7faa588d476fd70c366938b9
Kaleem <kaleem14you@gmail.com>
2024-07-10 12:53:54 +0500

    module demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { SignupComponent } from './user-auth/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
